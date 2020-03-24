a26c985e36b1e6148069cb5423cb7c03b0b1f958
Jacques Sham <jacquessham2015@gmail.com>
2020-03-24 14:35:40 -0700

    Finish the application script

--- RecommendationApplication/script.py
+++ RecommendationApplication/script.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def getWhiskyName(whiskynames:list):
 	numWhisky = len(whiskynames)
 
@@ -47,6 +50,79 @@ def getWhisky(whiskynames:list):
 	firstwhisky_index = whiskynames.index(firstwhisky)
 
 	return firstwhisky_index, firstwhisky
+
+def getWhiskyByFlavor(whisky_pdframe):
+	# Menu script
+	menuFlavor = "Please select the following option:\n"
+	menuFlavor += "1 - Body\n"
+	menuFlavor += "2 - Sweetness\n"
+	menuFlavor += "3 - Smoky\n"
+	menuFlavor += "4 - Medicinal\n"
+	menuFlavor += "5 - Tobacco\n"
+	menuFlavor += "6 - Honey\n"
+	menuFlavor += "7 - Spicy\n"
+	menuFlavor += "8 - Winey\n"
+	menuFlavor += "9 - Nutty\n"
+	menuFlavor += "10 - Malty\n"
+	menuFlavor += "11 - Fruity\n"
+	menuFlavor += "12 - Floral\n"
+	menuFlavor += "\n"
+	menuFlavor += "If you cannot decide, select '0'"
+
+	# List of flavor and strength
+	choicelist = ['Cannot decide','Body','Sweetness','Smoky','Medicinal',
+	              'Tobacco','Honey','Spicy','Winey','Nutty',
+	              'Fruity','Floral']
+	strengthlist = ['None','Light','Medium','Strong','Very strong']
+
+	# Obtain flavor
+	choice = -1
+	print(menuFlavor)
+	choice = int(input("Enter your choice:"))
+
+	while choice < 0 and choice > 12:
+		print("Invalid answer, please try again")
+		print(menuFlavor)
+		choice = int(input("Enter your choice:"))
+
+	print('You have choosen:', choicelist[choice])
+
+	# If user cannot decide, return None
+	if choice == 0:
+		return None
+
+	# Obtain Strength of flavor
+	menustrength = "Please the strength from 1 - 4, ranging from"
+	for strength in strengthlist:
+		menustrength += " "
+		menustrength += strength
+	menustrength += "."
+	print(menustrength)
+	strength_choice = int(input("Enter your choice:"))
+	while strength_choice < 1 and strength_choice > 4:
+		print("Invalid answer, please try again")
+		print(menustrength)
+		choice = int(input("Enter your choice:"))
+	print('You have choosen:', choicelist[choice])
+
+	print('You choice is:',strengthlist[strength_choice],choicelist[choice])
+
+	# Check if the strength of the flavor exist in the data set
+	uniquevalues = whisky_pdframe.iloc[:,choice].unique().tolist()
+	if strength_choice not in uniquevalues:
+		print('No such whisky, but we would tune down the strength')
+		# Find the next value below the original strength score
+		while strength_choice not in uniquevalues:
+			strength_choice -= 1
+		print('We will find',strengthlist[strength_choice],choicelist[choice],
+			  'for you.')
+
+	# Filter the list accordingly
+	recommendation = whisky_pdframe[whisky_pdframe.iloc[:,choice]==strength_choice]
+	recommendation = recommendation['Distillery'].tolist()
+
+	return recommendation
+
 
 menu = "Please select the following option:\n"
 menu += "1 - Start by entering whisky name\n"
--- RecommendationApplication/whisky_recommender.py
+++ RecommendationApplication/whisky_recommender.py
@@ -11,40 +11,50 @@ whiskydata = pd.read_csv("../whisky_with_cluster.csv")
 whiskynames = whiskydata['Distillery'].tolist()
 # Build name to cluster and cluster to name dictionaries
 whisky2group = dict(zip(whiskydata.Distillery.values, 
-	                    whiskydata.Cluster.values))
+                        whiskydata.Cluster.values))
 groups_list = whiskydata['Cluster'].unique().tolist()
 group2whisky = {}
 for group in groups_list:
-	group2whisky[group] = []
+    group2whisky[group] = []
 for name, cluster in zip(whiskydata.Distillery.values, 
-	                     whiskydata.Cluster.values):
-	group2whisky[cluster].append(name)
+                         whiskydata.Cluster.values):
+    group2whisky[cluster].append(name)
 
 # UI
 print(sc.menu)
 recommend_method = int(input("Enter your choice: "))
 while (recommend_method < 1 or recommend_method > 3):
-	print("Your input is invalid, please choose enter the valid option.")
-	print(sc.menu)
-	recommend_method  = int(input("Enter your choice:"))
+    print("Your input is invalid, please choose enter the valid option.")
+    print(sc.menu)
+    recommend_method  = int(input("Enter your choice:"))
 
 if recommend_method == 1:
-	firstwhisky_index, firstwhisky = sc.getWhisky(whiskynames)
-	cluster = whisky2group[firstwhisky]
+    firstwhisky_index, firstwhisky = sc.getWhisky(whiskynames)
+    cluster = whisky2group[firstwhisky]
 
-	firstwhisky_loc = [whiskydata.loc[whiskydata.index[firstwhisky_index],'Lat'],
-	                   whiskydata.loc[whiskydata.index[firstwhisky_index],'Lon']]
-	recommmendation = whiskydata[whiskydata['Cluster']==cluster]
-	recommmendation['distance'] = np.linalg.norm(recommmendation[['Lat','Lon']]
-		                            .sub(np.array(firstwhisky_loc)), axis=1)
-	recommmendation = recommmendation.sort_values(by=['distance'])
-	print("This is the list of recommmendation:")
-	print(recommmendation['Distillery'].tolist()[1:])
-
+    firstwhisky_loc = [whiskydata.loc[whiskydata.index[firstwhisky_index],'Lat'],
+                       whiskydata.loc[whiskydata.index[firstwhisky_index],'Lon']]
+    recommendation = whiskydata[whiskydata['Cluster']==cluster]
+    recommendation['distance'] = np.linalg.norm(recommendation[['Lat','Lon']]
+                                    .sub(np.array(firstwhisky_loc)), axis=1)
+    recommendation = recommendation.sort_values(by=['distance'])
+    # Remove the first element which is a duplicated entry
+    recommendation = recommendation['Distillery'].tolist()[1:]
 elif recommend_method == 2:
-	print("Feature is developing.")
-
+    recommendation = sc.getWhiskyByFlavor(whiskydata)
 elif recommend_method == 3:
-	print("Choose Macallan.")
+    recommendation = None
 
+# Display result
+if recommendation is not None:
+    if recommend_method == 1:
+        print("This is the list of recommendation in order:")
+    elif recommend_method == 2:
+        print("This is the list of recommendation in alphabetical order:")
+    counter = 1
+    for distillery in recommendation:
+        print(counter,'. ',distillery, sep='')
+        counter += 1
+else:
+    print("Choose Macallan.")
 
