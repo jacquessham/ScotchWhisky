140981926fbf5e27f6a1011181d35344beafef70
Jacques Sham <jacquessham2015@gmail.com>
2024-03-24 12:10:43 -0700

    Finish up the basic features on display_result

--- Lapointe_et_Legendre/Python/display_result.py
+++ Lapointe_et_Legendre/Python/display_result.py
@@ -5,6 +5,7 @@ def load_data():
 	# Read all files and prepare the data first
 	## Prepare distillery names
 	with open('../Data/Transformation/distillery.csv') as f:
+		# Remove header, first entry will have index=0
 		distilleries = f.read().splitlines()[1:]
 	dis2num = {}
 	num2dis = {}
@@ -47,26 +48,56 @@ def repeat_or_not():
 		return True
 	return False
 
+# Obtain user's input on his fav_whisky
+def pick_distillery(num2dis):
+	fav_whisky = None
+	while fav_whisky not in num2dis.keys():
+		print('Please select a distillery below:')
+		for k in num2dis.keys():
+			print(f'{k+1}: \t{num2dis[k]}')
+		fav_whisky = input('Your selection: ').strip()
+		try:
+			fav_whisky = int(fav_whisky)-1
+		except:
+			print('You selection is invalid, please try again!')
+			fav_whisky = None
+	return fav_whisky
+
+
+# Display recommendations in a beautified format
+def display_recommendation(fav_whisky, recommendations):
+	print(f'Here is the recommendations that is similar to {fav_whisky}:')
+	print(f'Order\tDistilleries')
+	for i, recommend in enumerate(recommendations):
+		print(f'{i+1}\t{recommend[0]}')
+
+
 # Return recommendation based on user's selection
 ## If there user's selection, use 84 (McCallan)
-def get_recommendation(results, num2dis, fav_whisky=84):
+def get_recommendation(results, num2dis, fav_whisky_num=84):
 	# Select a whisky first
-	selected = enumerate(results[fav_whisky].tolist())
+	selected = enumerate(results[fav_whisky_num].tolist())
 
 	# Display Result
-	recommendation = sorted(selected, key=lambda x: x[1])[1:6]
-	recommendation = [(num2dis[i], i, score) for i, score in recommendation]
-	print(recommendation)
+	recommendations = sorted(selected, key=lambda x: x[1])[1:6]
+	recommendations = [(num2dis[i], i, score) for i, score in recommendations]
+	# print(recommendations)
+	display_recommendation(num2dis[fav_whisky_num], recommendations)
 
 def interact(results, dis2num, num2dis):
 	repeat = True
 	while repeat:
 		menu_selection = homepage()
+		# Have user to pick a whisky
 		if menu_selection == 1:
-			print('Coming soon!')
+			fav_whisky = pick_distillery(num2dis)
+			print(f'The favourite whisky is {fav_whisky}')
+			get_recommendation(results, num2dis, fav_whisky)
+		# User has no where to get started
 		elif menu_selection == 2:
 			print('You may try McCallan! And also:')
 			get_recommendation(results, num2dis)
+		# Ask user if he wants to repeat the inquire again
 		repeat = repeat_or_not()
 
 def main():
